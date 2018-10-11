87b2f62a64b7b7a38e0cd7cb6c4bc69271f85259
Keishirou <36062945+Keishirou@users.noreply.github.com>
2018-10-11 17:45:46 +0900

    シリアル通信で割り込み処理していたスクリプトが機能しなくなる

--- Arduino/Unity_test_181005_serialEvent/Unity_test_181005_serialEvent.ino
+++ Arduino/Unity_test_181005_serialEvent/Unity_test_181005_serialEvent.ino
@@ -25,69 +25,68 @@ void loop() {
   int vr = analogRead(A2); //A2にボリューム部の2ピンを接続。1=5V,0=GND
   str = Serial.readStringUntil(';');
   slide = str.toInt();
-  Serial.println(slide);
-  //Serial.flush();
+  //Serial.println(slide);
+  Serial.flush();
 }
 
 //シリアル割り込み処理
 /*Unity側で行っている割り込み処理を重複させないための処理
   ArduinoがFを送信→Unity側からのシリアル通信を停止
   Arduinoがintを送信→Unity側からのシリアル通信を再開*/
-//void serialEvent() {
-//
-//  Serial.println("F"); //割り込み処理を重複させないためのフラグ
-//  if (Serial.available() > 0) {
-//    str = Serial.readStringUntil(';');
-//    Serial.flush();
-//    //str = Serial.readStringUntil('\n');
-//    //slide = Serial.read();
-//    slide = str.toInt();
-//
-//    /*伸縮処理開始*/
-//    int vr = analogRead(A2); //A2にボリューム部の2ピンを接続。1=5V,0=GND
-//
-//    if (vr > slide) {   // モーターA: 正転
-//      digitalWrite(12, HIGH); //正転
-//      digitalWrite(9, LOW); //ブレーキOFF
-//      analogWrite(3, SPEED1);
-//
-//      while (vr > slide) {  // 受信したデータよりスライダの位置が大きい場合
-//        vr = analogRead(A2);
-//        diff = vr - slide;
-//        if (-JITTER < diff && diff < JITTER) { // 一定の閾値以内であれば指定の位置まで移動したとみなす
-//          break;
-//        }
-//        /*割り込み処理で代替しようとしたけどこっちじゃないと即オーバーフローorガタガタ*/
-//        if ( Serial.available() ) {   // 上記で制止しなかったとしても，伸縮距離が変更されているかもしれないので再度データを受信
-//          String str = Serial.readStringUntil(';');
-//          Serial.flush();
-//          slide = str.toInt();
-//        }
-//        delay(1);
-//      }
-//      digitalWrite(9, HIGH); //ブレーキON
-//    } else if (vr < slide) {   // モーターA: 逆転
-//      digitalWrite(12, LOW);  //逆転
-//      digitalWrite(9, LOW); //ブレーキOFF
-//      analogWrite(3, SPEED2);
-//
-//      while (vr < slide) {  // 受信したデータよりスライダの位置が小さい場合
-//        vr = analogRead(A2);
-//        int diff = vr - slide;
-//        if (-JITTER < diff && diff < JITTER) { // 一定の閾値以内であれば指定の位置まで移動したとみなす
-//          break;
-//        }
-//    /*割り込み処理で代替しようとしたけどこっちじゃないと即オーバーフローorガタガタ*/
-//        if ( Serial.available() ) {   // 上記で制止しなかったとしても，伸縮距離が変更されているかもしれないので再度データを受信
-//          String str = Serial.readStringUntil(';');
-//          Serial.flush();
-//          slide = str.toInt();
-//        }
-//
-//        delay(1);
-//      }
-//      digitalWrite(9, HIGH);//ブレーキON
-//    }
-//  }
+void serialEvent() {
 
-//}
+  Serial.println("F"); //割り込み処理を重複させないためのフラグ
+  if (Serial.available() > 0) {
+    str = Serial.readStringUntil(';');
+    Serial.flush();
+    //str = Serial.readStringUntil('\n');
+    //slide = Serial.read();
+    slide = str.toInt();
+
+    /*伸縮処理開始*/
+    int vr = analogRead(A2); //A2にボリューム部の2ピンを接続。1=5V,0=GND
+
+    if (vr > slide) {   // モーターA: 正転
+      digitalWrite(12, HIGH); //正転
+      digitalWrite(9, LOW); //ブレーキOFF
+      analogWrite(3, SPEED1);
+
+      while (vr > slide) {  // 受信したデータよりスライダの位置が大きい場合
+        vr = analogRead(A2);
+        diff = vr - slide;
+        if (-JITTER < diff && diff < JITTER) { // 一定の閾値以内であれば指定の位置まで移動したとみなす
+          break;
+        }
+        /*割り込み処理で代替しようとしたけどこっちじゃないと即オーバーフローorガタガタ*/
+        if ( Serial.available() ) {   // 上記で制止しなかったとしても，伸縮距離が変更されているかもしれないので再度データを受信
+          String str = Serial.readStringUntil(';');
+          Serial.flush();
+          slide = str.toInt();
+        }
+        delay(1);
+      }
+      digitalWrite(9, HIGH); //ブレーキON
+    } else if (vr < slide) {   // モーターA: 逆転
+      digitalWrite(12, LOW);  //逆転
+      digitalWrite(9, LOW); //ブレーキOFF
+      analogWrite(3, SPEED2);
+
+      while (vr < slide) {  // 受信したデータよりスライダの位置が小さい場合
+        vr = analogRead(A2);
+        int diff = vr - slide;
+        if (-JITTER < diff && diff < JITTER) { // 一定の閾値以内であれば指定の位置まで移動したとみなす
+          break;
+        }
+    /*割り込み処理で代替しようとしたけどこっちじゃないと即オーバーフローorガタガタ*/
+        if ( Serial.available() ) {   // 上記で制止しなかったとしても，伸縮距離が変更されているかもしれないので再度データを受信
+          String str = Serial.readStringUntil(';');
+          Serial.flush();
+          slide = str.toInt();
+        }
+
+        delay(1);
+      }
+      digitalWrite(9, HIGH);//ブレーキON
+    }
+  }
+}
